d8f96006f2cb839ff5015c3088cdc773c7b04c3a
Ayush U <uttamayush1212@gmail.com>
2025-03-24 13:39:09 +0530

    assignment 2

---
+++ practical-assignment-2/1-area-circum-circle.cpp
@@ -0,0 +1,28 @@
+// 1=> write a c++ program to implement a class that called circle that has private member variable for radius include member function 
+//
+
+#include<iostream>
+using namespace std;
+class circle{
+      private:
+        float radius;
+        public:
+      circle(float r){
+          radius=r;
+      }
+      void area(){
+        cout<<"The area of the circle is: "<<3.14*radius*radius<<endl;
+      }
+      void circum(){
+        cout<<"The circumference of the circle is: "<<2*3.14*radius<<endl;
+      }
+};
+int main(){
+    float r;
+    cout<<"Enter the radius of the circle: ";
+    cin>>r;
+    circle c1(r);
+    c1.area();
+    c1.circum();
+    return 0;
+}
---
+++ practical-assignment-2/2-person-data.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class person {
+    private:
+    int age ;
+    string name;
+    string country;
+    public:
+    void set_data(){
+        cout<<"Enter the name of the person: ";
+        cin>>name;
+        cout<<"Enter the age of the person: ";
+        cin>>age;
+        cout<<"Enter the country of the person: ";
+        cin>>country;
+    }
+    void get_data(){
+        cout<<"The name of the person is: "<<name<<endl;
+        cout<<"The age of the person is: "<<age<<endl;
+        cout<<"The country of the person is: "<<country<<endl;
+    }
+};
+int main(){
+    person p1;
+    p1.set_data();
+    p1.get_data();
+    return 0;
+}
