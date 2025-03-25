1a51fcf022bfd1bb9a6ed4b30841463afb5951be
ayush-uttam732 <uttamayush894@gmail.com>
2025-03-25 09:43:27 +0530

    write 3 to 5 code

---
+++ practical-assignment-2/3-car-data.cpp
@@ -0,0 +1,30 @@
+//3 => Write a C++ program to create a class called Car that has private member variables
+// for company, model, and year. Implement member functions to get and set these  variables.
+#include<iostream> 
+using namespace std;
+class A{
+    private:
+    string company;
+    string model;
+    int year;
+    public:
+    void get_data(){
+        cout<<"Enter the company name : "<<endl;
+        cin>>company;
+        cout<<"Enter the model name : "<<endl;
+        cin>>model;
+        cout<<"Enter the year of manufacture : "<<endl<<endl;    ;
+        cin>>year;
+    }
+    void set_data(){
+        cout<<"Company name : "<<company<<endl;
+        cout<<"Model name : "<<model<<endl;
+        cout<<"Year of manufacture : "<<year<<endl;
+    }
+};
+int main(){
+    A obj;
+    obj.get_data();
+    obj.set_data();
+    return 0;
+}
